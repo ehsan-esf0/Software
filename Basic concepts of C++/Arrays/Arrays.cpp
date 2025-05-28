a16219d860f448ecb88a706b148fe563002b3bf7
ehsan <ehsanesfhany@gmail.com>
2025-05-28 13:30:40 +0430

    basic array in cpp

---
+++ Basic concepts of C++/Arrays/Arrays.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <array>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    // linear array
+    int numbers[10];
+
+    numbers[0] = 8;
+    numbers[1] = 2;
+    numbers[2] = 4;
+    numbers[3] = 6;
+
+    int arr[5] = {1, 2, 3, 4, 5};
+    int arr2[] = {10, 20, 30};
+    int arr3[5] = {};
+
+    // Multidimensional array
+    int matrix[3][4];
+    matrix[0][0] = 5;
+
+
+    // array
+    std::array<int, 5> arr4 = {1, 2, 3, 4, 5};
+
+    arr4[0] = 10;
+    arr4.at(2) = 30;
+
+    // vector
+    std::vector<int> v = {1, 2, 3};
+    v.push_back(4);
+    v[0] = 10;
+
+    int* dynamicArr = new int[10];
+    delete[] dynamicArr; 
+}
